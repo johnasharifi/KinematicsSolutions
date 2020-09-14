2d6f23aea1177ab6cf76b1cb5ff1c1a860841c65
johnasharifi <johnasharifi@gmail.com>
2020-09-13 23:11:13 -0700

    Add SetXYZOfTransform script
    Add script which sets xyz. Use with dynamic float sliders

---
+++ Scenes/Scripts/SetXYZOfTransform.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetXYZOfTransform : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float SetX
+    {
+        set
+        {
+            if (target != null)
+            {
+                target.position = new Vector3(value, target.position.y, target.position.z);
+            }
+        }
+    }
+
+    public float SetY
+    {
+        set
+        {
+            if (target != null)
+            {
+                target.position = new Vector3(target.position.x, value, target.position.z);
+            }
+        }
+    }
+
+    public float SetZ
+    {
+        set
+        {
+            if (target != null)
+            {
+                target.position = new Vector3(target.position.x, target.position.y, value);
+            }
+        }
+    }
+}
