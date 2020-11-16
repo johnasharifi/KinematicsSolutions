3eaa8c2395e887350ab2d1b60d2fd9286e2c02b8
johnasharifi <johnasharifi@gmail.com>
2020-11-16 13:58:02 -0800

    Eliminate IKNode.solving flag
    IKnode solving is now play mode only. This eliminates some issues which we encountered with edit-time ambiguity about what solve state to be in

--- InverseKinematicsLibrary/IKNode.cs
+++ InverseKinematicsLibrary/IKNode.cs
@@ -9,20 +9,7 @@ public class IKNode : MonoBehaviour
     public IKNode childNode { get; private set; }
 
     public Transform target;
-
-    [SerializeField] private bool solving = false;
-    public bool isSolving
-    {
-        get
-        {
-            return solving;
-        }
-        set
-        {
-            solving = value;
-        }
-    }
-
+    
     [SerializeField] private Vector3 minSpread;
     [SerializeField] private Vector3 maxSpread;
     
@@ -53,50 +40,44 @@ public class IKNode : MonoBehaviour
 
     public void Solve()
     {
-        // update and Editor loop.update both call Solve
-
-        // TODO in editor, check if editor is paused
-        if (solving)
+        if (target != null)
         {
-            if (target != null)
+            Vector3 intervention = (target.position - GetTerminus());
+            Vector3 maxIntervention = intervention;
+
+            float distanceDampener = Mathf.Pow(Mathf.Clamp(intervention.sqrMagnitude, 0.01f, maxDampDistance) / maxDampDistance, 0.5f);
+                
+            if (maxIntervention.sqrMagnitude < 1E-4)
             {
-                Vector3 intervention = (target.position - GetTerminus());
-                Vector3 maxIntervention = intervention;
-
-                float distanceDampener = Mathf.Pow(Mathf.Clamp(intervention.sqrMagnitude, 0.01f, maxDampDistance) / maxDampDistance, 0.5f);
-                
-                if (maxIntervention.sqrMagnitude < 1E-4)
-                {
-                    // generate a nonsense vector when at zero-magnitude error
-                    // because we cannot calculate a Quaternion.LookRotation for a zero-magnitude vector
-                    maxIntervention = Vector3.one;
-                }
-                maxIntervention.Normalize();
-                
-                Quaternion rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(maxIntervention), distanceDampener * Mathf.PI * rotationOverTimePenalty);
-                Quaternion localRotation = Quaternion.Inverse(transform.parent.rotation) * rotation;
-                
-                // extent to which child segments retrieve error from parents. 0 to ignore parent localRotation
-                localRotation = Quaternion.Lerp(localRotation, transform.parent.localRotation, echoRate * Time.deltaTime);
-
-                System.Func<float, float, float, float> eulerSpread = (float v, float bound1, float bound2) =>
-                {
-                    return Mathf.Clamp(v > 180f? v - 360f: v, Mathf.Min(bound1, bound2), Mathf.Max(bound1, bound2));
-                };
-                
-                Vector3 clampedEuler = new Vector3(
-                    eulerSpread(localRotation.eulerAngles.x, minSpread.x, maxSpread.x),
-                    eulerSpread(localRotation.eulerAngles.y, minSpread.y, maxSpread.y),
-                    eulerSpread(localRotation.eulerAngles.z, minSpread.z, maxSpread.z)
-                    );
-                
-                localRotation = Quaternion.Euler(clampedEuler);
-
-                // leak rate is HZ at an instant when playing / when FPS is calculable, 1 / 60 otherwise
-                float dt = (Application.isPlaying ? Time.deltaTime : 0.016f);
-                Quaternion leak = qres.Exchange(localRotation, dt);
-                transform.localRotation = leak;
+                // generate a nonsense vector when at zero-magnitude error
+                // because we cannot calculate a Quaternion.LookRotation for a zero-magnitude vector
+                maxIntervention = Vector3.one;
             }
+            maxIntervention.Normalize();
+                
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(maxIntervention), distanceDampener * Mathf.PI * rotationOverTimePenalty);
+            Quaternion localRotation = Quaternion.Inverse(transform.parent.rotation) * rotation;
+                
+            // extent to which child segments retrieve error from parents. 0 to ignore parent localRotation
+            localRotation = Quaternion.Lerp(localRotation, transform.parent.localRotation, echoRate * Time.deltaTime);
+
+            System.Func<float, float, float, float> eulerSpread = (float v, float bound1, float bound2) =>
+            {
+                return Mathf.Clamp(v > 180f? v - 360f: v, Mathf.Min(bound1, bound2), Mathf.Max(bound1, bound2));
+            };
+                
+            Vector3 clampedEuler = new Vector3(
+                eulerSpread(localRotation.eulerAngles.x, minSpread.x, maxSpread.x),
+                eulerSpread(localRotation.eulerAngles.y, minSpread.y, maxSpread.y),
+                eulerSpread(localRotation.eulerAngles.z, minSpread.z, maxSpread.z)
+                );
+                
+            localRotation = Quaternion.Euler(clampedEuler);
+
+            // leak rate is HZ at an instant when playing / when FPS is calculable, 1 / 60 otherwise
+            float dt = (Application.isPlaying ? Time.deltaTime : 0.016f);
+            Quaternion leak = qres.Exchange(localRotation, dt);
+            transform.localRotation = leak;
         }
     }
 
